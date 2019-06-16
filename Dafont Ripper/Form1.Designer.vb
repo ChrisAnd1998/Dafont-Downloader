919fbe84fb884272b0e16ad4a440254eba26f006
chris <chrisand1998@gmail.com>
2019-06-16 08:48:22 +0200

    Add project files.

---
+++ Dafont Ripper/Form1.Designer.vb
@@ -0,0 +1,479 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.Button24 = New System.Windows.Forms.Button()
+        Me.Button25 = New System.Windows.Forms.Button()
+        Me.Button26 = New System.Windows.Forms.Button()
+        Me.Button27 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button28 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 169)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(490, 38)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Rip All Fonts Categorized in A !"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(120, 163)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 213)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(490, 38)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Rip All Fonts Categorized in B !"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 257)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(490, 38)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Rip All Fonts Categorized in C !"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 301)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(490, 38)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "Rip All Fonts Categorized in D !"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 345)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(490, 38)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Rip All Fonts Categorized in E !"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 389)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(490, 38)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Rip All Fonts Categorized in F !"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 433)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(490, 38)
+        Me.Button7.TabIndex = 16
+        Me.Button7.Text = "Rip All Fonts Categorized in G !"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(12, 477)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(490, 38)
+        Me.Button8.TabIndex = 17
+        Me.Button8.Text = "Rip All Fonts Categorized in H !"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(508, 213)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(490, 38)
+        Me.Button9.TabIndex = 25
+        Me.Button9.Text = "Rip All Fonts Categorized in P !"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(508, 169)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(490, 38)
+        Me.Button10.TabIndex = 24
+        Me.Button10.Text = "Rip All Fonts Categorized in O !"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(12, 741)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(490, 38)
+        Me.Button11.TabIndex = 23
+        Me.Button11.Text = "Rip All Fonts Categorized in N !"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(12, 697)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(490, 38)
+        Me.Button12.TabIndex = 22
+        Me.Button12.Text = "Rip All Fonts Categorized in M !"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(12, 653)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(490, 38)
+        Me.Button13.TabIndex = 21
+        Me.Button13.Text = "Rip All Fonts Categorized in L !"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(12, 609)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(490, 38)
+        Me.Button14.TabIndex = 20
+        Me.Button14.Text = "Rip All Fonts Categorized in K !"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(12, 565)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(490, 38)
+        Me.Button15.TabIndex = 19
+        Me.Button15.Text = "Rip All Fonts Categorized in J !"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(12, 521)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(490, 38)
+        Me.Button16.TabIndex = 18
+        Me.Button16.Text = "Rip All Fonts Categorized in I !"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(508, 389)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(490, 38)
+        Me.Button17.TabIndex = 29
+        Me.Button17.Text = "Rip All Fonts Categorized in T !"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(508, 345)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(490, 38)
+        Me.Button18.TabIndex = 28
+        Me.Button18.Text = "Rip All Fonts Categorized in S !"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(508, 301)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(490, 38)
+        Me.Button19.TabIndex = 27
+        Me.Button19.Text = "Rip All Fonts Categorized in R !"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(508, 257)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(490, 38)
+        Me.Button20.TabIndex = 26
+        Me.Button20.Text = "Rip All Fonts Categorized in Q !"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(508, 565)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(490, 38)
+        Me.Button21.TabIndex = 33
+        Me.Button21.Text = "Rip All Fonts Categorized in X !"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Location = New System.Drawing.Point(508, 521)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(490, 38)
+        Me.Button22.TabIndex = 32
+        Me.Button22.Text = "Rip All Fonts Categorized in W !"
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Location = New System.Drawing.Point(508, 477)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(490, 38)
+        Me.Button23.TabIndex = 31
+        Me.Button23.Text = "Rip All Fonts Categorized in V !"
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'Button24
+        '
+        Me.Button24.Location = New System.Drawing.Point(508, 433)
+        Me.Button24.Name = "Button24"
+        Me.Button24.Size = New System.Drawing.Size(490, 38)
+        Me.Button24.TabIndex = 30
+        Me.Button24.Text = "Rip All Fonts Categorized in U !"
+        Me.Button24.UseVisualStyleBackColor = True
+        '
+        'Button25
+        '
+        Me.Button25.Location = New System.Drawing.Point(508, 653)
+        Me.Button25.Name = "Button25"
+        Me.Button25.Size = New System.Drawing.Size(490, 38)
+        Me.Button25.TabIndex = 35
+        Me.Button25.Text = "Rip All Fonts Categorized in Z !"
+        Me.Button25.UseVisualStyleBackColor = True
+        '
+        'Button26
+        '
+        Me.Button26.Location = New System.Drawing.Point(508, 609)
+        Me.Button26.Name = "Button26"
+        Me.Button26.Size = New System.Drawing.Size(490, 38)
+        Me.Button26.TabIndex = 34
+        Me.Button26.Text = "Rip All Fonts Categorized in Y !"
+        Me.Button26.UseVisualStyleBackColor = True
+        '
+        'Button27
+        '
+        Me.Button27.Location = New System.Drawing.Point(508, 697)
+        Me.Button27.Name = "Button27"
+        Me.Button27.Size = New System.Drawing.Size(490, 82)
+        Me.Button27.TabIndex = 36
+        Me.Button27.Text = "Rip All Fonts Categorized in # (NUMBERS) !"
+        Me.Button27.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(814, 20)
+        Me.TextBox1.TabIndex = 37
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button28)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(138, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(860, 48)
+        Me.GroupBox1.TabIndex = 38
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Download folder"
+        '
+        'Button28
+        '
+        Me.Button28.Location = New System.Drawing.Point(826, 17)
+        Me.Button28.Name = "Button28"
+        Me.Button28.Size = New System.Drawing.Size(27, 23)
+        Me.Button28.TabIndex = 38
+        Me.Button28.Text = "..."
+        Me.Button28.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.ProgressBar1)
+        Me.GroupBox2.Location = New System.Drawing.Point(138, 66)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(860, 97)
+        Me.GroupBox2.TabIndex = 39
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Progress"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(6, 67)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(848, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(10, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "-"
+        '
+        'Timer2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1010, 793)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button27)
+        Me.Controls.Add(Me.Button25)
+        Me.Controls.Add(Me.Button26)
+        Me.Controls.Add(Me.Button21)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.Button23)
+        Me.Controls.Add(Me.Button24)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Dafont.com Ripper written in VB.NET"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents Button25 As Button
+    Friend WithEvents Button26 As Button
+    Friend WithEvents Button27 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button28 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ Dafont Ripper/Form1.vb
@@ -0,0 +1,122 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.Threading
+
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim listy2 As New ListBox
+        Dim letter As String = "a"
+        Dim wc As New WebClient
+        Dim source As String = wc.DownloadString("https://www.dafont.com/alpha.php?lettre=" & letter & "&page=" & page & "&fpp=100")
+        Dim m2 As MatchCollection = Regex.Matches(source, "(?<=<a href=" & Chr(34) & ")[^" & Chr(34) & "]*", RegexOptions.Singleline + RegexOptions.IgnoreCase)
+        For Each m As Match In m2
+            Dim value As String = m.Groups(0).Value
+            If value.Contains("alpha.php?lettre=" & letter & "&page=") Then
+                listy2.Items.Add(value)
+            End If
+        Next
+        max = listy2.Items.Count + 1
+        amount = max * 100
+
+
+        Label2.Text = "About (" & amount & ") Fonts detected!"
+
+        ProgressBar1.Maximum = amount
+
+        Timer2.Start()
+
+        Dim thread As New Thread(AddressOf Rip_A)
+        thread.Start()
+    End Sub
+
+
+    Dim current As String
+    Dim letter As String
+    Dim amount As String
+    Dim max As String
+
+    Dim page As Integer
+    Dim ready As Boolean
+
+
+
+    Sub Rip_A()
+        ready = False
+        Dim listy As New ListBox
+        letter = "a"
+
+
+
+
+        Dim wc As New WebClient
+        Dim source As String = wc.DownloadString("https://www.dafont.com/alpha.php?lettre=" & letter & "&page=" & page & "&fpp=100")
+
+        Dim m1 As MatchCollection = Regex.Matches(source, "(http(s)?://)?([\w-]+\.)+[\w-]+(/[\w- ;,./?%&=]*)?", RegexOptions.Singleline + RegexOptions.IgnoreCase)
+        For Each m As Match In m1
+            Dim value As String = m.Groups(0).Value
+            If value.Contains("dl.dafont.com") Then
+                listy.Items.Add(value)
+            End If
+        Next
+
+        Try
+            For Each i As String In listy.Items
+
+                current = "http://" & i
+                My.Computer.Network.DownloadFile("http://" & i, TextBox1.Text & "\" & letter & "\" & i.Replace("dl.dafont.com/dl/?f=", "") & ".zip")
+
+
+            Next
+            listy.Items.Clear()
+        Catch
+        End Try
+        If page = max Then
+            page = 1
+            ready = True
+
+            Exit Sub
+        End If
+        page = page + 1
+        Rip_A()
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            If current = Nothing Then
+                Label1.Text = "-"
+            Else
+                Label1.Text = current
+            End If
+
+
+        Catch
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        ready = True
+
+    End Sub
+
+    Private Sub Button28_Click(sender As Object, e As EventArgs) Handles Button28.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Try
+            Dim fileCount As Integer = IO.Directory.GetFiles(TextBox1.Text & "\" & letter, "*.zip").Length
+            ProgressBar1.Value = fileCount
+        Catch
+        End Try
+
+    End Sub
+End Class
